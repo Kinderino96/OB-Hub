02441303b801e8bc51bab1df1f4f3ccfbeba6387
Kinderino96 <tratinpo@gmail.com>
2026-05-26 14:46:59 +0200

    Add files via upload

--- middleware.js
+++ middleware.js
@@ -1,11 +1,7 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
-
-export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) {
-    auth().protect();
-  }
+export default authMiddleware({
+  publicRoutes: ["/sign-in(.*)"],
 });
 
 export const config = {
